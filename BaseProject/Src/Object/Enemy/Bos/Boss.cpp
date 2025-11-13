e27dac041a79bdf4d3cd6844b76ee94a28d9372b
motineko-saka <5458m86845@gmail.com>
2025-11-13 15:05:16 +0900

    あれプッシュしてない？

---
+++ BaseProject/Src/Object/Enemy/Bos/Boss.cpp
@@ -0,0 +1,29 @@
+#include "Boss.h"
+
+Boss::Boss()
+{
+}
+
+Boss::~Boss()
+{
+}
+
+void Boss::Init(float _x, float _y, float _z)
+{
+}
+
+void Boss::Update()
+{
+}
+
+void Boss::Draw()
+{
+}
+
+void Boss::Release()
+{
+}
+
+void Boss::Kill()
+{
+}
--- BaseProject/Src/Object/Enemy/Bos/Boss.h
+++ BaseProject/Src/Object/Enemy/Bos/Boss.h
@@ -1,1 +1,31 @@
 #pragma once
+#include "../EnemyBase.h"
+
+class Boss : public EnemyBase 
+{
+public:
+    // コンストラクタ
+	Boss();
+
+	// デストラクタ
+	~Boss();
+
+    // 初期化
+    void Init(float _x, float _y, float _z) override;
+
+	// 更新
+    void Update() override;
+
+	// 描画
+    void Draw() override;
+
+	// 解放
+    void Release() override;
+
+	// 撃破
+    void Kill() override;      
+
+
+private:
+
+};
